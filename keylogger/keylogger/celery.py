02897f3f69a55200cb52b63b2f86ee221c59a54b
Samuel <samuelanand2005@gmail.com>
2025-03-12 01:25:25 +0530

    assigned

--- keylogger/keylogger/celery.py
+++ keylogger/keylogger/celery.py
@@ -1,6 +1,5 @@
 import os
 from celery import Celery
-from keymanagement import tasks
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'keylogger.settings')
 
@@ -8,4 +7,4 @@ app = Celery('keylogger')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.autodiscover_tasks(['keymanagement'])
+app.autodiscover_tasks()
